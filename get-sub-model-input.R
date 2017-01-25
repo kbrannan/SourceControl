510f236cc0f8d091883ffcfea1a84dee78f487e7
kbrannan <brannan.kevin@deq.state.or.us>
2017-01-25 11:20:52 -0800

    added commands to get the number of sub watersheds and contruct the data frame for output

--- get-sub-model-input.R
+++ get-sub-model-input.R
@@ -1,5 +1,5 @@
 
-chr.dir.sub.model <- df.sub.model.info[1,2]
+chr.dirs.sub.model <- df.sub.model.info[,2]
 chr.pat.sub.model.input <- "*\\.txt"
 
 chr.files.input <- list.files(
@@ -16,8 +16,18 @@ get.number.sub.wtsds <- function(chr.dir.sub.model,
   return(num.range.sub.wtsds)
 }
 
-get.number.sub.wtsds(chr.dir.sub.model, chr.pat.sub.model.input)
-
-range(do.call(rbind,
+num.number.of.sub.wtsds <- range(do.call(rbind,
         lapply(df.sub.model.info[,2], get.number.sub.wtsds, 
                chr.pat.sub.model.input)))
+
+df.sub.model.info
+
+df.sub.model.input <- cbind(df.sub.model.info[,1:2],
+                            array(data = NA, 
+                                  dim = c(length(df.sub.model.input[,1]),
+                                          max(num.number.of.sub.wtsds)),
+                                  dimnames = NULL))
+
+array(data = NA, dim = c(length(df.sub.model.input[,1]),
+                         max(num.number.of.sub.wtsds)),
+      dimnames = NULL)
