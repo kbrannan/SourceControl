990c08eae8377b3511a5c157abbad00a1ef607cb
kbrannan <brannan.kevin@deq.state.or.us>
2017-01-26 16:39:08 -0800

    put the code that runs the source sub model for all the sub watersheds in a seperate script file

--- run-sub-models-get-output.R
+++ run-sub-models-get-output.R
@@ -1,46 +1,94 @@
 ## run-sub-models-get-output
 
+
+## start function running for all sources
 lst.output <- list()
 
-ii <- 1
+## number of sub watersheds
+num.sub.wtsd <- length(df.sub.model.input.files[1, c(-1, -2)])
 
-chr.cur.sub.model.file <- paste0(df.sub.model.info[ii,2:3], collapse = "/")
+## number of sources
+num.sources <- length(df.sub.model.info$source)
 
-env.sub <- new.env()
-source(chr.cur.sub.model.file, local=env.sub)
-chr.cur.sub.model <- ls(envir = env.sub)
-rm(list=ls(envir = env.sub), envir = env.sub)
-
-vec.cur.sub.model.input <- paste0(df.sub.model.info$folder[ii], "/",
-                                  df.sub.model.input.files[ii, c(-1,-2)])
-
-chk.file <- function(f,p) {
-  output <- NA
-  if(is.na(f) != TRUE) paste0(p, "/", f)
+## loop for sources
+ii.src <- 1 ## iterator for source
+for(ii.src in 1:num.sources) {
+  
 }
 
-vec.cur.sub.model.input <- 
-  do.call(rbind, sapply(df.sub.model.input.files[ii, c(-1,-2)], 
-                        chk.file, df.sub.model.info$folder[ii]))
-do.call(cbind,dimnames(vec.cur.sub.model.input)[1])
 
-jj <- 1
+chr.cur.sub.model.src <- df.sub.model.info$source[ii.src]
+chr.cur.sub.model.dir <- df.sub.model.info$folder[ii.src]
+chr.cur.sub.model.file <- df.sub.model.info$model[ii.src]
+vec.sub.model.input.files <- df.sub.model.input.files[ii.src, c(-1,-2)]
 
-source(chr.cur.sub.model.file)
 
-myfun <- function(x, fun, f.env) {
-  output <- NA
-  if (is.character(fun)) func <- match.fun(fun)
-  if(file.exists(x)) output <- func(x)
-  return(output)
+
+### function here to run for source
+
+
+run.source.model.for.sub.wtds <- function(vec.sub.model.input.files,
+                                          chr.cur.sub.model.src, 
+                                          chr.cur.sub.model.dir,
+                                          chr.cur.sub.model.file) {
+  ## vec.sub.model.input.files - character vector of source model input files
+  ## chr.sub.model.src  - name of model R-script
+  ## chr.sub.model.dir  - source model R-script folder
+  ## chr.sub.model.file - source model R-script file
+  
+  
+  ## get name of sub-model function in the R-script file
+  env.sub <- new.env()
+  source(paste0(chr.cur.sub.model.dir, "/",
+                chr.cur.sub.model.file), local=env.sub)
+  chr.cur.sub.model <- ls(envir = env.sub)
+  rm(env.sub)
+  
+  ## get sub-model function
+  source(paste0(chr.cur.sub.model.dir, "/", chr.cur.sub.model.file))
+  
+  ## function wrapper to run source model using function name
+  fun.wrap.sub.model <- function(x, fun, f.env) {
+    ## set default output to NULL in case input file does not exist
+    output <- NULL
+    ## set a function to the named function in environment
+    if (is.character(fun)) func <- match.fun(fun)
+    ## if input file exists run function if not do nothing
+    if(file.exists(x)) output <- func(x)
+    ## output is either a data from from sub model function or NULL
+    return(output)
+  }
+  
+  ## input files with paths
+  vec.sub.model.input.files.with.paths <-
+    paste0(chr.cur.sub.model.dir, "/", vec.sub.model.input.files)
+  
+  ## run submodel for all sub-watersheds , returns a list of data frames
+  lst.cur.source <- lapply(vec.sub.model.input.files.with.paths, 
+                           fun.wrap.sub.model, chr.cur.sub.model)
+  
+  ## set names of the sub watershed data frames in output list
+  if(length(dimnames(vec.sub.model.input.files)[[1]]) == 
+     length(vec.sub.model.input.files)) { 
+    chr.sub.names <- dimnames(vec.sub.model.input.files)[[1]]
+  }
+  if(length(dimnames(vec.sub.model.input.files)[[2]]) == 
+     length(vec.sub.model.input.files)) { 
+    chr.sub.names <- dimnames(vec.sub.model.input.files)[[2]]
+  }
+  names(lst.cur.source) <- chr.sub.names
+  
+  ## return sub model for sources run for all sub watersheds
+  return(lst.cur.source)
 }
 
-lst.cur.source <- lapply(vec.cur.sub.model.input, myfun, chr.cur.sub.model)
-names(lst.cur.source) <- df.sub.model.info$source[ii]
+junk <- run.source.model.for.sub.wtds(vec.sub.model.input.files,
+                                      chr.cur.sub.model.src, 
+                                      chr.cur.sub.model.dir,
+                                      chr.cur.sub.model.file)
 
-names(lst.cur.source) <- do.call(cbind,dimnames(vec.cur.sub.model.input)[1])
+lst.output[[1]][chr.cur.sub.model.src] <- lst.cur.source
 
-rm(list = c(eval(chr.cur.sub.model)))
 
 lst.output[[length(lst.output) + 1]] <- 
   list(eval(
