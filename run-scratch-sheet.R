f50321b28c66df07c9903d11c21fd6ca947e7288
kbrannan <brannan.kevin@deq.state.or.us>
2017-02-22 12:05:54 -0800

    added commands to test getlim-load-to-pls.R and get-load-to-stream.R

--- run-scratch-sheet.R
+++ run-scratch-sheet.R
@@ -21,7 +21,18 @@ df.sub.model.input.files <-
 lst.output <- run.sub.models.for.sources.parallel(df.sub.model.info, 
                                          df.sub.model.input.files)
 
+ii.sub <- 12
+chr.pls <- "forest"
 ## get accum load
-df.load <- get.accum.load.to.pls(lst.output$sub.wtsd.names[3],
-                                 "forest", lst.output)
+df.load.accum <- get.accum.load.to.pls(lst.output$sub.wtsd.names[ii.sub],
+                                 chr.pls, lst.output)
 
+## get lim load
+df.load.lim <- get.lim.load.to.pls(lst.output$sub.wtsd.names[ii.sub],
+                                 chr.pls, lst.output)
+## check lim/accum ratio
+df.load.lim[,2] / df.load.accum[, 2]
+
+## get load to stream
+df.load.stream <- get.load.to.stream(lst.output$sub.wtsd.names[ii.sub], 
+                                     lst.output)
