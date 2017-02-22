90fe923b84cfede9fe425e4e01e08c8b0caa3d78
kbrannan <brannan.kevin@deq.state.or.us>
2017-02-22 10:57:16 -0800

    added comments in code and debugged the form of output data.frame and its names

--- get-accum-load-to-pls.R
+++ get-accum-load-to-pls.R
@@ -1,29 +1,40 @@
-## get-accum-load-to-pls
-
 get.accum.load.to.pls <- function(chr.sub.wtsd, chr.pls, lst.loads) {
+  ## function sums accum loads from all sources to a pls in a sub watersheds
+  ##
+  ## chr.sub.wtsd - character for sub-watershed in the loads list
+  ## chr.pls - character for the pls in the loads list for chr.sub.wtsd
+  ## lst.loads - list that contains names of the sources, names of 
+  ##             sub-watersheds all the data.frame outputs from source models.
+  ##             The list is output from run.sub.models.for.sources.parallel
   
+  ## prefix used to search source data.frames for accum loads
   chr.prefix <- "accum.*"
   
+  ## the number od sources in the loads list
   n.src <- length(lst.loads$source.names)
   
-  
+  ## create character vector of the names of months for use in load data.frame
   pct.mons <- as.POSIXlt(paste0("1999-",1:12,"-01"))
   
+  ## create empty data.frame for the loads to the pls from all the sources
   df.dest <- as.data.frame(
     setNames(replicate(13,character(0)),
              c("source", strftime(as.POSIXlt(paste0("1999-",1:12,"-01")), 
                                   format = "%b"))),
     stringsAsFactors = FALSE)
   
-  
+  ## loop through all the sources and populate the data.frame for the pls
   for(ii.src in 1:n.src) {
-    df.cur <- lst.loads[[lst.loads$source.names[ii.src]]][lst.loads$sub.wtsd.names[ii.sub]][[1]]
+    df.cur <- lst.loads[[lst.loads$source.names[ii.src]]][chr.sub.wtsd][[1]]
     col.cur <- grep(paste0(chr.prefix,chr.pls),names(df.cur), ignore.case = TRUE)
     if(length(col.cur) != 0) {
+  ## if the source has accum for pls extract from load data.frame
       chr.ld <- as.character(df.cur[,col.cur])
     } else {
+  ## if the source does not have accum for pls set to 0
       chr.ld <- rep("0", 12)
     }
+  ## add current source loads to pls to the data.frame for the loads
     df.dest <- 
       rbind(df.dest,
             eval(
@@ -35,107 +46,18 @@ get.accum.load.to.pls <- function(chr.sub.wtsd, chr.pls, lst.loads) {
                                      collapse = " ,"), 
                               ", stringsAsFactors = FALSE)")))
       )
+  ## clean up
     rm(df.cur, col.cur)
   }
+  ## sum across sources by month to get monthly load to pls
+  vec.loads <- colSums(sapply(df.dest[, 2:13], as.numeric))
+  df.loads <- data.frame(month = names(vec.loads),
+                          accum.load = vec.loads[],
+                          stringsAsFactors = FALSE,
+                         row.names = NULL)
   
+  names(df.loads) <- c("month", paste0(gsub("\\.\\*", "", chr.prefix), ".to.",chr.pls))
   
-  df.loads <- colSums(sapply(df.dest[, 2:13], as.numeric))
-  
+  ##  return monthly loads data.frame as function output
+  return(df.loads)
 }
-
-chr.dest <- c("Pasture", "Forest", "RAOCUT", "stream")
-n.dest <- length(chr.dest)
-
-
-ii.sub <- 8
-ii.dest <- 1
-
-lst.loads <- lst.output
-chr.sub.wtsd <- lst.loads$sub.wtsd.names[ii.sub]
-chr.pls <- chr.dest[ii.dest]
-
-
-
-
-
-
-
-
-lst.output[source.names == "Cow.Calf", sub.wtsd.names == "sub.wtsd.01", ]
-
-names(lst.output)
-
-lst.output$source.names
-lst.output$sub.wtsd.names
-
-
-## get the load to pasture in sub wtsd 01
-
-ii <- 5
-n.src <- length(lst.output$source.names)
-
-ii.dest <- 1
-
-pct.mons <- as.POSIXlt(paste0("1999-",1:12,"-01"))
-strftime(as.POSIXlt(paste0("1999-",1:12,"-01")), format = "%b")
-
-df.dest <- as.data.frame(
-  setNames(replicate(13,character(0)),
-           c("source", strftime(as.POSIXlt(paste0("1999-",1:12,"-01")), format = "%b"))))
-
-for(ii.src in 1:n.src) {
-  Sys.sleep(0.1)
-  df.cur <- lst.output[[lst.output$source.names[ii.src]]][lst.output$sub.wtsd.names[ii.sub]][[1]]
-  col.cur <- grep(paste0("accum.*",chr.dest[ii.dest]),names(df.cur), ignore.case = TRUE)
-  if(length(col.cur) != 0) {
-    chr.ld <- as.character(df.cur[,col.cur])
-  } else {
-    chr.ld <- rep("0", 12)
-  }
-  
-  print(paste0("Values for ", lst.output$source.names[ii.src], " in ", 
-         lst.output$sub.wtsd.names[ii.sub], " on ", chr.dest[ii.dest], " is ",
-         paste0(chr.ld, collapse = " ")))
-  print("")
-  flush.console()
-  df.dest <- 
-    rbind(df.dest,
-          eval(
-            parse(
-              text = paste0("data.frame(",
-                            paste0(names(df.dest), " = '", 
-                                   c(lst.output$source.names[ii.src],
-                                     chr.ld),"'",
-                                   collapse = " ,"), ")")))
-    )
-}
-
-colSums(as.numericdf.dest[, 2:13])
-
-rbind(df.dest,df.now)
-df.now <- eval(parse(text = paste0("data.frame(",
-       paste0(names(df.dest), " = '", c(lst.output$source.names[ii.src],df.cur[,col.cur]),"'",
-       collapse = " ,"), ")")))
-
-c("data.frame(",paste0(names(df.dest), " = '", c(lst.output$source.names[ii.src],df.cur[,col.cur]), "'"), ")")
-
-
-
-chr.dest <- c("Pasture", "Forest", "RAOCUT", "stream")
-
-length(lst.output[lst.output$sub.wtsd.names[ii.sub.wtsd]])
-
-df.cur <- lst.output[[lst.output$source.names[vec.sources[ii]]]][[lst.output$sub.wtsd.names[ii.sub.wtsd]]]
-
-paste0(lst.output$source.names[ii.src], ": ", 
-       paste0(names(lst.output[[lst.output$source.names[ii.src]]]
-                    [[lst.output$sub.wtsd.names[ii.sub.wtsd]]]), sep = " "))
-
-grep("Accum.", names(df.cur), ignore.case = TRUE, value = TRUE)
-
-
-int.cols <- grep("Accum.Pasture", names(df.cur), ignore.case = TRUE)
-
-df.cur[, int.cols]
-
-lst.output[["Cow.Calf"]]
