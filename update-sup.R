e1023d6f439d729ea466e592208da82ff6b41f40
kbrannan <brannan.kevin@deq.state.or.us>
2017-03-28 15:49:55 -0700

    sorting out the link between the output from the models and the functions that identifies the lines in the sup file. function not complete yet

--- update-sup.R
+++ update-sup.R
@@ -3,9 +3,29 @@ chr.file <- paste0(chr.dir.hspf, "/", chr.file.uci)
 chr.input.hspf.sup <- scan(file = chr.file,
                            what = "character", sep = "\n",
                            quiet = TRUE)
-df.cur <- lst.loads[["sub.wtsd.01"]]
+df.cur.loads <- lst.loads[["sub.wtsd.01"]]
 
-df.cur$load[df.cur$load.to == "Pasture"][1]
+chr.sub.wtsd <- unique(gsub("[^0-9]", "", df.cur.loads$sub.wtsd))
+df.cur.sup.lines <- df.sup.lines[grep(paste0("^", chr.sub.wtsd, "$"), 
+                                      df.sup.lines$sub), ]
+
+df.cur.sup.lines$pls.name <- gsub("[dD]evel.*", "RAOCUT", as.character(df.cur.sup.lines$pls.name))
+
+df.cur.sup.lines$pls.name <- tolower(df.cur.sup.lines$pls.name)
+df.cur.sup.lines <- unique(df.cur.sup.lines[, ])
+
+df.cur.accum <- df.cur.loads[grep("accum", df.cur.loads$hspf.input), ]
+df.cur.accum$load.to <- tolower(df.cur.accum$load.to)
+
+chr.load.to <- unique(tolower(df.cur.accum$load.to))
+
+unique(df.cur.accum[grep("forest", tolower(df.cur.accum$load.to)), c("load.to", "month", "load")])
+
+merge(unique(df.cur.accum[, c("load.to", "month", "load")]),
+      unique(df.cur.sup.lines[, c("pls.name", "sup.num.accum")]),
+      by.x = c("load.to"), by.y = c("pls.name"))
+
+grep("^24", chr.file)
 
 ##  3.0614881E+06
 paste0(sprintf(fmt = "  %.5E", df.cur$load[df.cur$load.to == "Pasture"]),
