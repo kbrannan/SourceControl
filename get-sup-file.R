fa993a769b6db96a0ae770d89e868ed0d7ac7276
kbrannan <brannan.kevin@deq.state.or.us>
2017-05-02 15:58:02 -0700

    finished function. it returns a single data frame with the accum and sqolim tables in long format with the first column is name and second is value

--- get-sup-file.R
+++ get-sup-file.R
@@ -1,52 +1,44 @@
-## Source control scripts folder 
-chr.dir.source.control.scripts <- 
-  "M:/Models/Bacteria/HSPF/bacteria-sub-model-testing/SourceControl"
-
-## hspf simulation folder
-chr.dir.hspf <- paste0(chr.dir.source.control.scripts, "/hspf")
-
-## hspf suplimental file name
-chr.file.sup <- "bigelkwq.sup"
-
-
-
-options(stringsAsFactors = FALSE)
-
-chr.sup <- scan(file = paste0(chr.dir.hspf, "/", chr.file.sup),
-             what = "character",
-             sep = "\n", quiet = TRUE)
-
-
-
-lng.accum.st <- grep("MON-ACCUM", chr.sup)
-lng.sqolim.st <- grep("MON-SQOLIM", chr.sup)
-
-lng.accum.blk <- c(lng.accum.st, lng.sqolim.st - 1)
-lng.sqolim.blk <- c(lng.sqolim.st, length(chr.sup))
-
-chr.sup.accum <- chr.sup[lng.accum.blk[1]:lng.accum.blk[2]]
-
-chr.sup.sqolim <- chr.sup[lng.sqolim.blk[1]:lng.sqolim.blk[2]]
-
-
-make.mon.df <- function(chr.table, chr.pre = "") {
-
-  chr.sup.line <- gsub("(( ){1,}12)|[^0-9]","",
-                       chr.table[seq(from = 1, 
-                                               to = length(chr.table), 
-                                               by = 2)])
-  df.table <- as.data.frame(
-    cbind(
-      strftime(as.POSIXct(paste0("1967-",1:12,"-1")), format = "%b"),
-      do.call(cbind,
-              strsplit(chr.table[
-                seq(from = 2, to = length(chr.table), by = 2)],
-                split = "( ){1,}"))[-1,]))
+get.sup <- function(chr.file) {
+  options(stringsAsFactors = FALSE)
   
-  names(df.table) <- c("month", paste0(chr.pre, "supln",chr.sup.line))  
-  return(df.table)
+  chr.sup <- scan(file = chr.file,
+                  what = "character",
+                  sep = "\n", quiet = TRUE)
+  lng.accum.st <- grep("MON-ACCUM", chr.sup)
+  lng.sqolim.st <- grep("MON-SQOLIM", chr.sup)
+  lng.accum.blk <- c(lng.accum.st, lng.sqolim.st - 1)
+  lng.sqolim.blk <- c(lng.sqolim.st, length(chr.sup))
+  chr.sup.accum <- chr.sup[lng.accum.blk[1]:lng.accum.blk[2]]
+  chr.sup.sqolim <- chr.sup[lng.sqolim.blk[1]:lng.sqolim.blk[2]]
+  make.mon.df <- function(chr.table, chr.pre = "") {
+    
+    chr.sup.line <- gsub("(( ){1,}12)|[^0-9]","",
+                         chr.table[seq(from = 1, 
+                                       to = length(chr.table), 
+                                       by = 2)])
+    df.table <- as.data.frame(
+      cbind(
+        strftime(as.POSIXct(paste0("1967-",1:12,"-1")), format = "%b"),
+        do.call(cbind,
+                strsplit(chr.table[
+                  seq(from = 2, to = length(chr.table), by = 2)],
+                  split = "( ){1,}"))[-1,]))
+    
+    names(df.table) <- c("month", paste0(chr.pre, "supln",chr.sup.line))  
+    return(df.table)
+  }
+  df.accum <- make.mon.df(chr.sup.accum, "accum")
+  df.sqolim <- make.mon.df(chr.sup.sqolim, "sqolim")
+  make.long <- function(df.cur) {
+    df.sup <- data.frame(character(0), character(0))  
+    for(ii in 2:length(names(df.cur))) {
+      df.sup <- rbind(df.sup,
+                      cbind(paste0(names(df.cur)[ii], df.cur$month), 
+                            df.cur[, ii]))
+    }
+    df.sup <- data.frame(df.sup)
+    names(df.sup) <- c("varname", "value")
+    return(df.sup)
+  }
+  df.sup <- rbind(make.long(df.accum), make.long(df.sqolim))
 }
-
-df.accum <- make.mon.df(chr.sup.accum, "accum")
-df.sqolim <- make.mon.df(chr.sup.sqolim, "sqolim")
-
