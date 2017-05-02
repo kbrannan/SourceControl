4a161cbd3728542ce7b5b4787d399a487782dd84
kbrannan <brannan.kevin@deq.state.or.us>
2017-05-02 09:10:18 -0700

    creating a function to read the sup file and extract the mon-accum and mon-sqolim tables to create input for PEST

--- get-sup-file.R
+++ get-sup-file.R
@@ -1,9 +1,33 @@
-junk <- scan(file = paste0(chr.dir.hspf, "/", chr.file.sup),
+## Source control scripts folder 
+chr.dir.source.control.scripts <- 
+  "M:/Models/Bacteria/HSPF/bacteria-sub-model-testing/SourceControl"
+
+## hspf simulation folder
+chr.dir.hspf <- paste0(chr.dir.source.control.scripts, "/hspf")
+
+## hspf suplimental file name
+chr.file.sup <- "bigelkwq.sup"
+
+
+chr.sup <- scan(file = paste0(chr.dir.hspf, "/", chr.file.sup),
              what = "character",
              sep = "\n", quiet = TRUE)
 
 
 
-grep("MON-ACCUM", junk)
+lng.accum.st <- grep("MON-ACCUM", chr.sup)
+lng.sqolim.st <- grep("MON-SQOLIM", chr.sup)
 
-grep("MON-SQOLIM", junk)
+lng.accum.blk <- c(lng.accum.st, lng.sqolim.st - 1)
+lng.sqolim.blk <- c(lng.sqolim.st, length(chr.sup))
+
+chr.sup.accum <- chr.sup[lng.accum.blk[1]:lng.accum.blk[2]]
+
+chr.sup.sqolim <- chr.sup[lng.sqolim.blk[1]:lng.sqolim.blk[2]]
+
+gsub("(( ){1,}12)|[^0-9]","",chr.sup.accum[seq(from = 1, to = length(chr.sup.accum), by = 2)])
+
+
+do.call(rbind,strsplit(chr.sup.accum[seq(from = 1, to = length(chr.sup.accum), by = 2)],
+         split = "( ){1,}"))
+chr.sup.accum[seq(from = 2, to = length(chr.sup.accum), by = 2)]
