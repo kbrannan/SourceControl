e2478aa353cfb6c0890cbd9f4952f200d23777c4
kbrannan <brannan.kevin@deq.state.or.us>
2017-06-07 14:21:46 -0700

    created functions that use parallel functions to get loads. this increased the speeds of the get load functions by 50%

---
+++ wrapper-get-loads-parallel.R
@@ -0,0 +1,23 @@
+wrapper.get.loads.parallel <- function(chr.prefix, chr.pls.names, lst.output,
+                                       chr.dir.source.control.scripts) {
+  library(data.table)
+  library(parallel)
+  source(paste0(chr.dir.source.control.scripts, "/",
+                "get-loads-parallel.R"))
+  
+
+  try(stopCluster(c1), silent = TRUE)
+  ## create cluster
+  no_cores <- detectCores() - 1
+  if(no_cores < 1) no_cores <- 1
+  c1 <- makeCluster(no_cores)
+  ## export functions to cluster
+  clusterExport(c1, lsf.str())
+
+  ## run quick.get.parallel and return a data frame of loads  
+  df.loads <- rbindlist(parLapply(c1, lst.output$sub.wtsd.names, get.loads.parallel,
+                                              chr.prefix, chr.pls.names, lst.output,
+                                              chr.dir.source.control.scripts))
+  
+  return(df.loads)
+}
